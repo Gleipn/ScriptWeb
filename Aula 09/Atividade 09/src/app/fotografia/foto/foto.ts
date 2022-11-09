dddde803f01bda314d276093253552a6eeede654
Allaf <allaflima@hotmail.com>
2022-11-09 19:14:38 -0300

    Atividade 09

---
+++ Aula 09/Atividade 09/src/app/fotografia/foto/foto.ts
@@ -0,0 +1,9 @@
+//Criamos nossa interface
+//A interface vai tipar os do banco de dados
+//Ela vai trabalhar entre o array imagens e os dados vindo da API
+
+export interface Fotos {
+  id: number;
+  img: string;
+  titulo: string;
+}
---
+++ Aula 09/Atividade 09/src/app/fotografia/fotografia.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { HomeComponent } from './home/home.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { FotoComponent } from './foto/foto.component';
+
+@NgModule({
+  //Determina quais componentes são responsabilidade do modulo
+  declarations: [FotoComponent, HomeComponent, FormularioComponent],
+  imports: [BrowserModule, FormsModule],
+  //Permite que outros modulos ou componentes acessem estre grupo
+  exports: [FotoComponent, HomeComponent, FormularioComponent],
+})
+export class FotografiaModule {}
